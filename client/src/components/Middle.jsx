6c25956db0a37a57ec6ae6d38cd340f675c4f248
Muskan <muskantolani7@gmail.com>
2023-09-07 00:51:08 +0530

    cards updated

--- client/src/components/Middle.jsx
+++ client/src/components/Middle.jsx
@@ -1,52 +1,63 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
-
 
 const Middle = () => {
   return (
     <>
-    <div className='top-mid'>
-        
-        <h2 className='name'>History</h2>
-        <div className='card'>
-        <div className='content-t-m'>
-        <div className="card-body">
-            <h5 className="card-title">26 August</h5>
-            <p className="card-text"><br />Location: Lonavala <br />Duration: 2 Days</p> <br />
+      <div className="top-mid">
+        <h2 className="name">History</h2>
+        <div className="card">
+          <div className="content-t-m">
+            <div className="card-body">
+              <span className="first-content">26 August</span>
+              <p className="second-content">
+                <br />
+                Location: Lonavala <br />
+                Duration: 2 Days
+              </p>{" "}
+              <br />
+            </div>
+            <button className="Third-content">Repeat Trip</button>
+            <br />
+            <button className="Third-content">Details</button>
+          </div>
+          <div className="content-t-m">
+            <div className="card-body">
+              <span className="first-content">2 October</span>
+              <p className="second-content">
+                <br />
+                Location: Nashik <br />
+                Duration: 2 Days
+              </p>{" "}
+              <br />
+            </div>
+            <button className="Third-content">Repeat Trip</button>
+            <br />
+            <button className="Third-content">Details</button>
+          </div>
+          <div className="content-t-m">
+            <div className="card-body">
+              <span className="first-content">17 August</span>
+              <p className="second-content">
+                <br />
+                Location: Karjat <br />
+                Duration: 2 Days
+              </p>
+              <br />
+            </div>
+            <button className="Third-content">Repeat Trip</button>
+            <br />
+            <button className="Third-content">Details</button>
+          </div>
         </div>
-        <button className="my-button">Repeat Trip</button><br />
-        <button className="my-button">Details</button>
-        </div>
-        <div className='content-t-m'>
-        <div className="card-body">
-            <h5 className="card-title">2 October</h5>
-            <p className="card-text"><br />Location: Nashik <br />Duration: 2 Days</p> <br />
-        </div>
-        <button className="my-button">Repeat Trip</button><br />
-        <button className="my-button">Details</button>
-        </div>
-        <div className='content-t-m'>
-        <div className="card-body">
-            <h5 className="card-title">17 August</h5>
-            <p className="card-text"><br />Location: Karjat <br />Duration: 2 Days</p><br />
-        </div>
-        <button className="my-button">Repeat Trip</button><br />
-        <button className="my-button">Details</button>
-        </div>
-        
-    </div>
-    
-</div>
-    <div className='bottom-mid'>
-    <button className='tripbutton'>
-    <span>
-        PLAN NOW
-    </span>
-</button>
-    </div>
-    
+      </div>
+      <div className="bottom-mid">
+        <button className="tripbutton">
+          <span>PLAN NOW</span>
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Middle
+export default Middle;
